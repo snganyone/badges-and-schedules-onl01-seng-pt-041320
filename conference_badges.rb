2c77140c7ca4a36dd8c41912b8ceab4ac76aa011
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-17 02:13:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(array)
   return arr
 end
 
-def printer
-  puts batch_badge_creator(["Josh"])
-  puts assign_rooms(["Josh"])
+def printer(array)
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
 end
